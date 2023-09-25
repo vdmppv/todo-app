9efd1bd015097df727297a3308c28624cb5930d7
vdmppv <ejafjadlajokudl@gmail.com>
2023-09-25 22:18:44 +0300

    Added select (checkbox), multiple items deletion

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { List, Button } from 'antd';
+import { List, Button, Badge, Checkbox } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeTodo, updateTodo } from '../slices/todoSlice';
 import Todo from '../models/Todo';
@@ -7,12 +7,14 @@ import TodoForm from './TodoForm';
 
 const TodoList: React.FC = () => {
   const [editingTodo, setEditingTodo] = useState<Todo | null>(null);
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
   const todos = useSelector((state: {todo: {todos: Todo[]}}) => state.todo.todos);
   const dispatch = useDispatch();
 
   const handleRemove = (id: number[]) => {
     dispatch(removeTodo(id));
+    setSelectedItems([]);
   };
 
   const handleEdit = (todo: Todo) => {
@@ -28,9 +30,27 @@ const TodoList: React.FC = () => {
     setEditingTodo(null);
   };
 
+  const handleTodoToggle = (id: number) => {
+    setSelectedItems((prevSelected) => {
+      const index = prevSelected.indexOf(id);
+      if (index !== -1) {
+        return prevSelected.filter((itemId) => itemId !== id);
+      } else {
+        return [...prevSelected, id];
+      }
+    });
+  };
+
   return (
     <List
       dataSource={todos}
+      header={
+        selectedItems.length ? 
+          <Button onClick={handleRemove.bind(this, selectedItems)}>
+            Delete selected <Badge count={selectedItems.length} />
+          </Button>
+        : null
+      }
       renderItem={(item: Todo) => (
         <List.Item
           key={item.id}
@@ -43,6 +63,7 @@ const TodoList: React.FC = () => {
             <TodoForm todo={item} onSave={handleSaveEdit} onCancel={handleCancelEdit} />
           ) : (
             <>
+              <Checkbox checked={selectedItems.indexOf(item.id) !== -1} onChange={() => handleTodoToggle(item.id)} />
               <List.Item.Meta title={item.description} description={item.date} style={{marginLeft: 20}} />
             </>
           )}
