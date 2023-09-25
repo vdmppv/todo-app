21eb527bc380de08e3491196376064128bec9b06
vdmppv <ejafjadlajokudl@gmail.com>
2023-09-25 21:56:44 +0300

    created redux store

---
+++ src/models/Todo.ts
@@ -0,0 +1,7 @@
+interface Todo {
+  id: number;
+  description: string;
+  date: string;
+}
+
+export default Todo;
---
+++ src/slices/todoSlice.ts
@@ -0,0 +1,55 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import Todo from '../models/Todo';
+
+interface TodoState {
+  todos: Todo[];
+}
+
+const initialState: TodoState = {
+  todos: [
+    {
+      id: 1,
+      description: 'Learn React',
+      date: '18/08/2023',
+    },
+    {
+      id: 2,
+      description: 'Learn TypeScript',
+      date: '21/09/2023',
+    },
+    {
+      id: 3,
+      description: 'Learn Ant Design',
+      date: '22/09/2023',
+    },
+    {
+      id: 4,
+      description: 'Learn Redux',
+      date: '24/09/2023',
+    }
+  ],
+};
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      state.todos.push(action.payload);
+    },
+    removeTodo: (state, action: PayloadAction<number[]>) => {
+      state.todos = state.todos.filter(todo => !action.payload.includes(todo.id));
+    },
+    editTodo: (state, action: PayloadAction<Todo>) => {
+      const { id, description, date } = action.payload;
+      const existingTodo = state.todos.find(todo => todo.id === id);
+      if (existingTodo) {
+        existingTodo.description = description;
+        existingTodo.date = date;
+      }
+    }
+  },
+});
+
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions;
+export default todoSlice.reducer;
